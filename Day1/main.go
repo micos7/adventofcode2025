6bd8dc30fcbba83de9ac90ee44767b63a772dd17
Mihai <micos7@gmail.com>
2025-12-03 11:06:44 +0200

    cleanup

--- Day1/main.go
+++ Day1/main.go
@@ -7,30 +7,6 @@ import (
 	"strconv"
 )
 
-func floorDiv(a, b int) int {
-	// Mathematical floor(a / b) for integers
-	// Works for positive and negative a.
-	if b <= 0 {
-		panic("b must be positive in floorDiv")
-	}
-	if a >= 0 {
-		return a / b
-	}
-	// For negative a, (a / b) in Go truncates toward zero.
-	// floor(a/b) = - ((-a + b - 1) / b)
-	return -(((-a) + b - 1) / b)
-}
-
-func countMultiplesInRange(lower, upper, m int) int {
-	// Count integers k such that m*k is in [lower, upper].
-	// If lower>upper return 0.
-	if lower > upper {
-		return 0
-	}
-	// count = floor(upper/m) - floor((lower-1)/m)
-	return floorDiv(upper, m) - floorDiv(lower-1, m)
-}
-
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -75,46 +51,34 @@ func main() {
 	// Part 2 (fixed)
 	posP2 := 50
 	part2Password := 0
+
 	for _, line := range rotations {
 		if line == "" {
 			continue
 		}
+
 		dir := line[0]
 		stepsVal, _ := strconv.Atoi(line[1:])
-		steps := stepsVal
+
+		move := 1
 		if dir == 'L' {
-			steps = -steps
+			move = -1
 		}
 
-		start := posP2
-		end := start + steps
+		for i := 0; i < stepsVal; i++ {
+			posP2 += move
 
-		var lower, upper int
-		if steps > 0 {
-			// Moving right: we travel (start, end] => integers start+1 ... end
-			lower = start + 1
-			upper = end
-		} else if steps < 0 {
-			// Moving left: we travel [end, start) => integers end ... start-1
-			lower = end
-			upper = start - 1
-		} else {
-			// zero steps => nothing crossed
-			lower = 1
-			upper = 0 // ensures lower>upper => count 0
+			if posP2%100 == 0 {
+				part2Password++
+			}
 		}
 
-		// Count multiples of 100 in [lower, upper]
-		part2Password += countMultiplesInRange(lower, upper, 100)
-
-		// Update position modulo 100
-		posP2 = end % 100
+		posP2 = posP2 % 100
 		if posP2 < 0 {
 			posP2 += 100
 		}
 	}
 
-	fmt.Println("--- Advent of Code Day 1 ---")
 	fmt.Println("Password (Part 1 - Final Landing on 0):", part1Password)
 	fmt.Println("Password (Part 2 - Total Clicks on 0):", part2Password)
 }
