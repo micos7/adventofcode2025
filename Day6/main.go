1125b55ec5ba7684b5417b09367180e83f564885
Mihai <micos7@gmail.com>
2025-12-12 17:32:00 +0200

    day 6 part 1

--- Day6/main.go
+++ Day6/main.go
@@ -11,56 +11,129 @@ import (
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
-		fmt.Println("Error opening file: Please make sure 'input.txt' is in the current directory.")
+		fmt.Println("Error opening file:", err)
 		return
 	}
-
 	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
-	ans := 0
-	var rows [][]int
-	var ops []rune
+
+	var dataLines []string
+	var opLine string
 
 	for scanner.Scan() {
-		line := strings.TrimSpace(scanner.Text())
-		// fmt.Println(line)
-		fields := strings.Fields(line)
-
-		if len(fields) > 0 && (fields[0] == "+" || fields[0] == "*") {
-			for _, o := range fields {
-				ops = append(ops, rune(o[0]))
-			}
+		line := scanner.Text()
+		if len(line) == 0 {
 			continue
 		}
+		if strings.ContainsAny(line, "+*") {
+			opLine = line
+		} else {
+			dataLines = append(dataLines, line)
+		}
+	}
 
-		var nums []int
+	part1Numbers := make([][]int, len(dataLines))
+	for i, line := range dataLines {
+		fields := strings.Fields(line)
+		nums := []int{}
 		for _, f := range fields {
-			n, err := strconv.Atoi(f)
-			if err != nil {
-				fmt.Println("Error parsing ", err)
-				return
-			}
+			n, _ := strconv.Atoi(f)
 			nums = append(nums, n)
 		}
-		rows = append(rows, nums)
-
+		part1Numbers[i] = nums
 	}
 
-	cols := len(rows[0])
+	part1Total := 0
+	if len(part1Numbers) > 0 && len(part1Numbers[0]) > 0 {
+		cols := len(part1Numbers[0])
+		opFields := strings.Fields(opLine)
 
-	for c := range cols {
-		operation := ops[c]
-		value := rows[0][c]
-		for _, row := range rows[1:] {
-			if operation == '+' {
-				value += row[c]
-			} else if operation == '*' {
-				value *= row[c]
+		currentOps := make([]string, len(opFields))
+		copy(currentOps, opFields)
+
+		if len(currentOps) < cols {
+			for i := len(currentOps); i < cols; i++ {
+				currentOps = append(currentOps, "+")
 			}
 		}
-		ans += value
+
+		for c := 0; c < cols; c++ {
+			val := part1Numbers[0][c]
+			operator := currentOps[c]
+
+			for r := 1; r < len(part1Numbers); r++ {
+				if operator == "+" {
+					val += part1Numbers[r][c]
+				} else if operator == "*" {
+					val *= part1Numbers[r][c]
+				}
+			}
+			part1Total += val
+		}
+	}
+	fmt.Println("Part 1 total:", part1Total)
+
+	maxLen := 0
+	for _, line := range dataLines {
+		if len(line) > maxLen {
+			maxLen = len(line)
+		}
+	}
+	if len(opLine) > maxLen {
+		maxLen = len(opLine)
 	}
 
-	fmt.Println(ans)
+	grid := make([][]rune, len(dataLines))
+	for r, line := range dataLines {
+		runes := []rune(line)
+		for len(runes) < maxLen {
+			runes = append(runes, ' ')
+		}
+		grid[r] = runes
+	}
 
+	opRunes := []rune(opLine)
+	for len(opRunes) < maxLen {
+		opRunes = append(opRunes, ' ')
+	}
+
+	ans2 := 0
+	currentGroupNums := []int{}
+
+	for col := maxLen - 1; col >= 0; col-- {
+		numStr := ""
+		for r := 0; r < len(grid); r++ {
+			char := grid[r][col]
+			if char >= '0' && char <= '9' {
+				numStr += string(char)
+			}
+		}
+
+		opChar := opRunes[col]
+		hasNumber := len(numStr) > 0
+		isOpColumn := (opChar == '+' || opChar == '*')
+
+		if hasNumber {
+			num, _ := strconv.Atoi(numStr)
+			currentGroupNums = append(currentGroupNums, num)
+		}
+
+		if isOpColumn {
+			if len(currentGroupNums) > 0 {
+				val := currentGroupNums[0]
+				for i := 1; i < len(currentGroupNums); i++ {
+					if opChar == '+' {
+						val += currentGroupNums[i]
+					} else if opChar == '*' {
+						val *= currentGroupNums[i]
+					}
+				}
+				ans2 += val
+				currentGroupNums = []int{}
+			}
+		}
+	}
+
+	fmt.Println("Part 2 ", ans2)
 }
